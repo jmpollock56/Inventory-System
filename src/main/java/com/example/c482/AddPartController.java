443a9bf347320f2972288d151b400ffb1ac26df9
jmpolllock56 <jmpollock50@gmail.com>
2023-09-20 17:36:27 -0400

    --

--- src/main/java/com/example/c482/AddPartController.java
+++ src/main/java/com/example/c482/AddPartController.java
@@ -7,14 +7,13 @@ import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.scene.control.RadioButton;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.stage.Stage;
 
 
 import java.io.IOException;
 import java.net.URL;
+import java.util.Optional;
 import java.util.Random;
 import java.util.ResourceBundle;
 
@@ -24,13 +23,14 @@ public class AddPartController  implements Initializable {
    @FXML public RadioButton inHouseRadio;
     @FXML public RadioButton outsourcedRadio;
     @FXML public Label changeText;
-    @FXML public TextField partId;
-    @FXML public TextField partName;
-    @FXML public TextField partInv;
-    @FXML public TextField partCost;
-    @FXML public TextField partMax;
-    @FXML public TextField partMin;
-    @FXML public TextField partSwitch;
+    @FXML public TextField id;
+
+    @FXML public TextField name;
+    @FXML public TextField inventory;
+    @FXML public TextField cost;
+    @FXML public TextField min;
+    @FXML public TextField max;
+    @FXML public TextField switchField;
     int generatedPartId;
     int generatedProductId;
 
@@ -59,22 +59,66 @@ public class AddPartController  implements Initializable {
     }
 
 
-    public void savePart(ActionEvent actionEvent) {
+    public void savePart(ActionEvent actionEvent) throws IOException {
 
         Random random = new Random();
 
         int randomId = random.nextInt(9000) + 1000;
         if (inHouseRadio.isSelected()){
-            String partNameVal = partName.getText();
-            int partInvVal = Integer.parseInt(partInv.getText());
-            int partCostVal = Integer.parseInt(partCost.getText());
-            int partMaxVal = Integer.parseInt(partMax.getText());
-            int partMinVal = Integer.parseInt(partMin.getText());
-            int partMachineId = Integer.parseInt(partSwitch.getText());
+            String partNameVal = name.getText();
+            int partInvVal = Integer.parseInt(inventory.getText());
+            double partCostVal = Double.parseDouble(cost.getText());
+            int partMaxVal = Integer.parseInt(max.getText());
+            int partMinVal = Integer.parseInt(min.getText());
+            int partMachineId = Integer.parseInt(switchField.getText());
+            System.out.println(partMinVal);
+            System.out.println(partMaxVal);
 
-            Inventory.addPart(new InHouse(1, "name", 1,1,1,1,1));
+            if (partMinVal > partMaxVal){
+                Alert alert = new Alert(Alert.AlertType.WARNING);
+                alert.setTitle("Minimum is higher than Maximum");
+                alert.setHeaderText("Warning");
+                alert.setContentText("The Minimum stock should be lower than the Maximum stock");
+
+                alert.getButtonTypes().setAll(ButtonType.OK);
+
+                Optional<ButtonType> result = alert.showAndWait();
+
+                return;
+
+            }
+
+            Inventory.addPart(new InHouse(randomId, partNameVal, partCostVal,partInvVal,partMinVal,partMaxVal,partMachineId));
+
+            Parent mainForm = FXMLLoader.load(getClass().getResource("main-form.fxml"));
+            Scene mainScene = new Scene(mainForm);
+            Stage mainStage = (Stage) ((Node)actionEvent.getSource()).getScene().getWindow();
+            mainStage.setScene(mainScene);
+            mainStage.show();
+
         } else if (outsourcedRadio.isSelected()) {
+            String productNameVal = name.getText();
+            int productInvVal = Integer.parseInt(inventory.getText());
+            double productCostVal = Double.parseDouble(cost.getText());
+            int productMaxVal = Integer.parseInt(max.getText());
+            int productMinVal = Integer.parseInt(min.getText());
+            String productCompanyName = switchField.getText();
 
+            if (productMinVal > productMaxVal){
+                Alert alert = new Alert(Alert.AlertType.WARNING);
+                alert.setTitle("Minimum is higher than Maximum");
+                alert.setHeaderText("Warning");
+                alert.setContentText("The Minimum stock should be lower than the Maximum stock");
+
+                alert.getButtonTypes().setAll(ButtonType.OK);
+
+                Optional<ButtonType> result = alert.showAndWait();
+
+                return;
+
+            }
+
+            Inventory.addPart(new Outsourced(randomId, productNameVal, productCostVal,productInvVal,productMinVal,productMaxVal,productCompanyName));
         }
     }
 }
